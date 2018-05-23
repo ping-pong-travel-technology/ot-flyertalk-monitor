09ca262ee215e3a44152206f0f60ced1aa13e9d0
Josh Ourisman <josh@joshourisman.com>
2018-05-23 11:25:34 -0400

    Better selecting of elements, capture post ID as well.

--- monitor.py
+++ monitor.py
@@ -12,10 +12,16 @@ soup = BeautifulSoup(html, 'html.parser')
 threadslist = soup.find(id='threadslist')
 threads = threadslist.find_all(class_='trow text-center')
 
+
+def title_link(tag):
+    return tag.has_attr('id') and tag['id'].startswith('thread_title_')
+
+
 thread_data = [
     {
-        'title': thread.find_all('div')[3].a.string,
-        'url': thread.find_all('div')[3].a['href'],
+        'id': thread.find(title_link)['id'].rsplit('_', 1)[1],
+        'title': thread.find(title_link).string,
+        'url': thread.find(title_link)['href'],
         'started': thread.find_all('div')[4].find_all('span')[-1].string,
     } for thread in threads
 ]
@@ -24,4 +30,5 @@ for thread in thread_data:
         thread['started'].split(' ', 1)[1]
     ).datetime().isoformat()
     title = thread['title']
-    print(f'{started} - {title}')
+    id = thread['id']
+    print(f'{started} - {id} - {title}')
