8ca39ddcc360f826f238c893de815abd8288b548
Josh Ourisman <me@josho.io>
2022-04-19 15:51:38 +0000

    Progress indicators make no sense, just use log statements.

--- monitor.py
+++ monitor.py
@@ -8,7 +8,6 @@ import requests
 from bs4 import BeautifulSoup
 from pydantic import BaseSettings, HttpUrl
 from rich.console import Console
-from rich.progress import Progress, SpinnerColumn, TimeElapsedColumn
 from sqlmodel import Field, Session, SQLModel, create_engine, select
 
 
@@ -40,90 +39,73 @@ def main():
     settings = Settings()
     console = Console()
 
-    with Progress(
-        "[progress.description]{task.description}", SpinnerColumn(), TimeElapsedColumn()
-    ) as progress:
-        fetch_task = progress.add_task(
-            "Fetching threads from https://www.flyertalk.com/", total=1
+    console.log("Fetching threads from https://www.flyertalk.com/")
+    URL_PREFIX = "https://www.flyertalk.com/forum/"
+    THREADS_URL = f"{URL_PREFIX}search.php?do=finduser&u=24793&starteronly=1"
+
+    response = requests.get(str(THREADS_URL))
+
+    console.log("Parsing HTML data for threads")
+    html = response.content
+    soup = BeautifulSoup(html, "html.parser")
+
+    threadslist = soup.find(id="threadslist")
+    threads = threadslist.find_all(class_="trow text-center")
+
+    def title_link(tag):
+        return tag.has_attr("id") and tag["id"].startswith("thread_title_")
+
+    console.log(f"Processing {len(threads)} threads")
+    thread_data = []
+    for thread in threads:
+        thread_data.append(
+            {
+                "id": thread.find(title_link)["id"].rsplit("_", 1)[1],
+                "title": thread.find(title_link).string,
+                "url": URL_PREFIX + thread.find(title_link)["href"],
+                "started": thread.find_all("div")[4].find_all("span")[-1].string,
+                "seen": pendulum.now(),
+            }
         )
-        URL_PREFIX = "https://www.flyertalk.com/forum/"
-        THREADS_URL = f"{URL_PREFIX}search.php?do=finduser&u=24793&starteronly=1"
 
-        response = requests.get(str(THREADS_URL))
-        progress.update(fetch_task, completed=1)
+    new = set()
+    with Session(engine) as session:
+        console.log("Checking database for existing threads")
+        for thread in thread_data:
+            thread_id = thread["id"]
+            statement = select(Thread).where(Thread.id == thread_id)
+            result = session.exec(statement)
 
-        parse_task = progress.add_task("Parsing HTML data for threads", total=1)
-        html = response.content
-        soup = BeautifulSoup(html, "html.parser")
+            if result.first() is None:
+                session.add(Thread(**thread))
+                new.add(thread_id)
 
-        threadslist = soup.find(id="threadslist")
-        threads = threadslist.find_all(class_="trow text-center")
-        progress.update(parse_task, completed=1)
+        session.commit()
 
-        def title_link(tag):
-            return tag.has_attr("id") and tag["id"].startswith("thread_title_")
-
-        process_task = progress.add_task(
-            f"Processing {len(threads)} threads", total=len(threads)
-        )
-        thread_data = []
-        for thread in threads:
-            thread_data.append(
-                {
-                    "id": thread.find(title_link)["id"].rsplit("_", 1)[1],
-                    "title": thread.find(title_link).string,
-                    "url": URL_PREFIX + thread.find(title_link)["href"],
-                    "started": thread.find_all("div")[4].find_all("span")[-1].string,
-                    "seen": pendulum.now(),
-                }
-            )
-            progress.update(process_task, advance=1)
-
-        new = set()
+    if settings.POST_TO_SLACK is True and len(new) > 0:
+        console.log(f"Sending notifications for {len(new)} thread(s)")
         with Session(engine) as session:
-            check_task = progress.add_task(
-                "Checking database for existing threads", total=len(threads)
-            )
-            for thread in thread_data:
-                thread_id = thread["id"]
+            for thread_id in new:
                 statement = select(Thread).where(Thread.id == thread_id)
                 result = session.exec(statement)
+                thread = result.one()
 
-                if result.first() is None:
-                    session.add(Thread(**thread))
-                    new.add(thread_id)
-
-                progress.update(check_task, advance=1)
-
-            session.commit()
-
-        if settings.POST_TO_SLACK is True and len(new) > 0:
-            notification_task = progress.add_task(
-                f"Sending notifications for {len(new)} thread(s)", total=len(new)
-            )
-            with Session(engine) as session:
-                for thread_id in new:
-                    statement = select(Thread).where(Thread.id == thread_id)
-                    result = session.exec(statement)
-                    thread = result.one()
-
-                    try:
-                        requests.post(
-                            str(settings.WEBHOOK_URL),
-                            json.dumps(
-                                {
-                                    "text": f"David posted to FlyerTalk: {thread.title} - {thread.url}"
-                                }
-                            ),
-                            headers={"content-type": "application/json"},
-                        )
-                    except Exception:
-                        pass
-                    else:
-                        thread.notified = True
-                        session.add(thread)
-                        session.commit()
-                progress.update(notification_task, advance=1)
+                try:
+                    requests.post(
+                        str(settings.WEBHOOK_URL),
+                        json.dumps(
+                            {
+                                "text": f"David posted to FlyerTalk: {thread.title} - {thread.url}"
+                            }
+                        ),
+                        headers={"content-type": "application/json"},
+                    )
+                except Exception:
+                    pass
+                else:
+                    thread.notified = True
+                    session.add(thread)
+                    session.commit()
 
 
 if __name__ == "__main__":
