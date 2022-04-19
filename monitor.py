5ad26c7522768cf5010c83c9dad7d626121a3466
Josh Ourisman <me@josho.io>
2022-04-19 15:25:44 +0000

    Replaced SQLiteDict with SQLModel.

--- monitor.py
+++ monitor.py
@@ -1,13 +1,32 @@
 import json
+from datetime import datetime
+from pathlib import Path
 from typing import Optional
 
 import pendulum
 import requests
 from bs4 import BeautifulSoup
 from pydantic import BaseSettings, HttpUrl
-from sqlitedict import SqliteDict
+from sqlmodel import Field, Session, SQLModel, create_engine, select
 
-db = SqliteDict("threads.sqlite", encode=json.dumps, decode=json.loads)
+
+class Thread(SQLModel, table=True):
+    id: int = Field(default=None, primary_key=True)
+    title: str
+    url: str
+    started: str
+    seen: Optional[datetime]
+    notified: Optional[bool] = False
+
+
+sqlite_file_name = "threads.db"
+sqlite_url = f"sqlite:///{sqlite_file_name}"
+
+engine = create_engine(sqlite_url)
+
+
+def create_db_and_tables():
+    SQLModel.metadata.create_all(engine)
 
 
 class Settings(BaseSettings):
@@ -37,44 +56,52 @@ def main():
             "title": thread.find(title_link).string,
             "url": URL_PREFIX + thread.find(title_link)["href"],
             "started": thread.find_all("div")[4].find_all("span")[-1].string,
-            "seen": pendulum.now().to_iso8601_string(),
-            "notified": False,
+            "seen": pendulum.now(),
         }
         for thread in threads
     ]
 
     new = set()
-    for thread in thread_data:
-        thread_id = thread["id"]
+    with Session(engine) as session:
+        for thread in thread_data:
+            thread_id = thread["id"]
+            statement = select(Thread).where(Thread.id == thread_id)
+            result = session.exec(statement)
 
-        if thread_id not in db:
-            db[thread_id] = thread
-            new.add(thread_id)
+            if result.first() is None:
+                session.add(Thread(**thread))
+                new.add(thread_id)
+
+        session.commit()
 
     if len(new) > 0:
-        for thread_id in new:
-            thread = db[thread_id]
-            thread_title = thread["title"]
-            thread_url = thread["url"]
-            if settings.POST_TO_SLACK is True:
-                try:
-                    requests.post(
-                        str(settings.WEBHOOK_URL),
-                        json.dumps(
-                            {
-                                "text": f"David posted to FlyerTalk: {thread_title} - {thread_url}"
-                            }
-                        ),
-                        headers={"content-type": "application/json"},
-                    )
-                except Exception:
-                    pass
-                else:
-                    thread["notified"] = True
-                    db[thread_id] = thread
+        with Session(engine) as session:
+            for thread_id in new:
+                statement = select(Thread).where(Thread.id == thread_id)
+                result = session.exec(statement)
+                thread = result.one()
 
-    db.commit()
+                if settings.POST_TO_SLACK is True:
+                    try:
+                        requests.post(
+                            str(settings.WEBHOOK_URL),
+                            json.dumps(
+                                {
+                                    "text": f"David posted to FlyerTalk: {thread.title} - {thread.url}"
+                                }
+                            ),
+                            headers={"content-type": "application/json"},
+                        )
+                    except Exception:
+                        pass
+                    else:
+                        thread.notified = True
+                        session.add(thread)
+                        session.commit()
 
 
 if __name__ == "__main__":
+    if Path(sqlite_file_name).exists() is False:
+        create_db_and_tables()
+
     main()
